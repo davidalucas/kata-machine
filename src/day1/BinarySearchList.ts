80b0d328ac4d9ff3d87f8e0bfc60983b33786f5e
David Lucas <davidalucas92@gmail.com>
2023-12-27 12:44:45 -0500

    Implements binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  // we're assuming haystack is sorted here
+  // if you're not sure, you can check it first with a little for-loop, but it adds O(n) time
 
+  let lo = 0;
+  let hi = haystack.length - 1;
+
+  while (lo <= hi) {
+    // const mid = Math.floor((lo + hi) / 2); // this causes issues in JS for large numbers
+    const mid = lo + Math.floor((hi - lo) / 2); // this makes sure you don't accidentally overflow
+
+    if (haystack[mid] === needle) {
+      return true;
+    } else if (haystack[mid] < needle) {
+      lo = mid + 1;
+    } else {
+      hi = mid - 1;
+    }
+  }
+  return false;
 }
