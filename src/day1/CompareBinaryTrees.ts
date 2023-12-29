003bafc2b92b0119e43ff8ce18c015b209e4c811
David Lucas <davidalucas92@gmail.com>
2023-12-29 16:32:53 -0500

    Implemented the binary tree comparison algorithm

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,23 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+function walk(
+  a: BinaryNode<number> | null,
+  b: BinaryNode<number> | null,
+): boolean {
+  // termination conditions
+  if (a === null && b === null) {
+    return true;
+  } else if (a === null || b === null) {
+    return false;
+  } else if (a.value !== b.value) {
+    return false;
+  }
 
+  return walk(a!.left, b!.left) && walk(a!.right, b!.right);
+}
+
+export default function compare(
+  a: BinaryNode<number> | null,
+  b: BinaryNode<number> | null,
+): boolean {
+  // remember DFS preserves shape, while BFS does not
+  return walk(a, b);
 }
