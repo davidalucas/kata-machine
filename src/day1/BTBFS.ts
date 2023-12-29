7670452a6c605ed2e4fe13c26f5b6d7a96034e72
David Lucas <davidalucas92@gmail.com>
2023-12-29 16:09:58 -0500

    Implements breadth-first search for binary tree

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,22 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+import Queue from "./Queue";
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  // always remember that breadth-first search just needs a queue, not recursion
+  // start by pushing head to the queue, then do a while loop until the queue is empty
+  const myQueue = new Queue<BinaryNode<number>>();
+  myQueue.enqueue(head);
+
+  while (myQueue.length > 0) {
+    const node = myQueue.deque();
+    if (node?.value === needle) {
+      return true;
+    }
+    if (node?.left) {
+      myQueue.enqueue(node?.left);
+    }
+    if (node?.right) {
+      myQueue.enqueue(node?.right);
+    }
+  }
+  return false;
 }
