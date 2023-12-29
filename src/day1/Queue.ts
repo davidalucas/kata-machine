86d7ba358026d3015dcf2ac889788adf11e249ff
David Lucas <davidalucas92@gmail.com>
2023-12-29 16:02:20 -0500

    Implements queue data structure

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,47 @@
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.length = 0;
+    this.head = this.tail = undefined;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    if (this.length === 0) {
+      this.head = this.tail = { value: item };
+    } else {
+      this.tail!.next = { value: item };
+      this.tail = this.tail!.next;
     }
+    this.length++;
+  }
 
-    enqueue(item: T): void {
+  deque(): T | undefined {
+    let value: T | undefined;
+    if (this.length === 0) {
+      return undefined;
+    } else if (this.length === 1) {
+      value = this.head!.value;
+      this.head = this.tail = undefined;
+    } else {
+      value = this.head!.value;
+      this.head = this.head!.next;
+    }
+    this.length--;
+    return value;
+  }
 
-}
-    deque(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
+  peek(): T | undefined {
+    if (this.length === 0) {
+      return undefined;
+    }
+    return this.head!.value;
+  }
 }
